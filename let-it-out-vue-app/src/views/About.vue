6fd06924b4d8b3fd7502def8a43469f9dca73573
Adrian Hagjija <adrianhagjija@hotmail.com>
2021-02-21 20:07:00 +0100

    top bar added and logo

--- let-it-out-vue-app/src/views/About.vue
+++ let-it-out-vue-app/src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>YOOOO</h1>
   </div>
 </template>
+<script>
+export default {
+  name: "About",
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
